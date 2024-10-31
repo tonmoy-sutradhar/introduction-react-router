ba47ba7949977f3ffea563644d53d4fb263d56e5
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-10-31 22:13:31 +0600

    use Navigation for loading...

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import "./Home.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 const Home = () => {
+  const navigation = useNavigation();
   return (
     <div className="font">
       <Header></Header>
-      <Outlet></Outlet>
+      {navigation.state === "loading" ? <p>Loading...</p> : <Outlet></Outlet>}
+
       <Footer></Footer>
     </div>
   );
